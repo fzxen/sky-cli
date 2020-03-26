1f5a4f9c050e7f3c6cab6c1262e2f291f670ed6a
fanzhongxu <zx_fan@outlook.com>
2020-03-26 17:02:32 +0800

    feat: init

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const { Command } = require('commander')
+const program = new Command()
+
+const commands = {
+  create: {},
+  init: {},
+  dev: {},
+  build: {}
+}
+
+console.log('hello sky-cli')
